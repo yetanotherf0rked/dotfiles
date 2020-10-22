bed9bdaadb67388a5723ea98b21ace3ed2fdef8b
Malki Nawfal <nawfal.malki@insa-cvl.fr>
2020-10-22 20:46:39 +0200

    powerline config

--- updateconfig.sh
+++ updateconfig.sh
@@ -4,3 +4,11 @@ mv ~/.vimrc ~/.vimrcbackup
 ln -S .bashrc ~
 ln -S .vimrc ~
 ln -S .vimrc ~/.config/nvim/init.vim
+mv
+~/.local/lib/python3.8/site-packages/powerline/config_files/colorschemes/default.json
+~/.local/lib/python3.8/site-packages/powerline/config_files/colorschemes/default.json.backup
+mv
+~/.local/lib/python3.8/site-packages/powerline/config_files/themes/shell/default.json
+~/.local/lib/python3.8/site-packages/powerline/config_files/themes/shell/default.json.backup
+ln -S .config/powerline/colorschemes/themes/shell/default.json
+ln -S .config/powerline/themes/shell/default.json
